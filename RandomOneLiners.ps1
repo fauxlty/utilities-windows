90d4ed900b05b587a17ef3d5cdeaed9a6922ba9c
fauxlty <flake_canopy0v@icloud.com>
2022-06-20 15:32:19 -0700

    More additions to the one-liners script

--- RandomOneLiners.ps1
+++ RandomOneLiners.ps1
@@ -2,29 +2,29 @@ Get-Service -Name TermService | Format-Table -AutoSize
 
 Invoke-Command { netstat -ano | findstr -i 3389 }
 
-reg add "\\S07497NT0154US.wmoui.wal-mart.com\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0x0 /f
+reg add "\\$server\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0x0 /f
 
 Invoke-Command { reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0x0 /f }
 
 Invoke-Command { netsh advfirewall firewall set rule group="remote desktop" new enable=Yes }
 
-Test-NetConnection -ComputerName S07497NT0154US.wmoui.wal-mart.com -CommonTCPPort RDP
+Test-NetConnection -ComputerName $server -CommonTCPPort RDP
 
-Compare-Object -ReferenceObject (AdFind.exe -h wal-mart.com -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq) -DifferenceObject (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC=Wal-Mart, DC=com member -list) -IncludeEqual
+Compare-Object -ReferenceObject (AdFind.exe -h $domain -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq) -DifferenceObject (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC+$Domain, DC=com member -list) -IncludeEqual
 
-Compare-Object -ReferenceObject (AdFind.exe -h wal-mart.com -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq) -DifferenceObject (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC=Wal-Mart, DC=com member -list) -ExcludeDifferent
+Compare-Object -ReferenceObject (AdFind.exe -h $domain -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq) -DifferenceObject (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC+$Domain, DC=com member -list) -ExcludeDifferent
 
-# AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC=Wal-Mart,DC=com member -list
+# AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC+$Domain,DC=com member -list
 
-# AdFind.exe -h wal-mart.com -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq
+# AdFind.exe -h $domain -f "&(objectcategory=organizationalunit)(name=domain controllers)" -dsq | AdFind.exe -s onelevel -f objectcategory=computer -dsq
 
-(AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC=Wal-Mart, DC=com member -list -mvsort) | measure-object
+(AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC+$Domain, DC=com member -list -mvsort) | measure-object
 
-$dcs = (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC=Wal-Mart, DC=com member -list -mvsort); $dcs
+$dcs = (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Admins, DC=US, DC+$Domain, DC=com member -list -mvsort); $dcs
 
-# AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC=Wal-Mart,DC=com -vmetal
+# AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC+$Domain,DC=com -vmetal
 
-#dn:CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC=Wal-Mart,DC=com
+#dn:CN=ADDS-GPOACL-DomainControllerIsolation,OU=Groups,OU=Admins,DC=US,DC+$Domain,DC=com
 
 #Example: I have an entry in keepass that the name is Reset Passwords with the following in the notes
 
@@ -48,7 +48,7 @@ $dcs = (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Ad
 # HomeOffice.Wal-Mart.com
 # 	adfind -f samaccountname=cm-User -dsq | admod unicodepwd::"Password" -optenc
 # Wal-Mart
-# 	adfind -h wal-mart.com -f samaccountname=SA-User -dsq | admod -h wal-mart.com unicodepwd::"Password" -optenc
+# 	adfind -h $domain -f samaccountname=SA-User -dsq | admod -h $domain unicodepwd::"Password" -optenc
 # =========================================================================================================
 # Execute from Paw
 # RunDMZ (PAW)
@@ -58,13 +58,13 @@ $dcs = (AdFind.exe -b CN=ADDS-GPOACL-DomainControllerIsolation, OU=Groups, OU=Ad
 # 	adfind -f samaccountname=User -dsq | admod unicodepwd::"Password" -optenc
 
 #here is the format using for /f -- for ref as well lol -- since alldc is only on the jumps
-#for /f %i in ('adfind -hh wal-mart.com -sc dclist') do adfind -hh %i -rootdse currenttime -alldcd -list
+#for /f %i in ('adfind -hh $domain -sc dclist') do adfind -hh %i -rootdse currenttime -alldcd -list
 
 # FYI - here is some syntax using alldc (instead for for /f) for quickly checking a set of dc's, etc
-# alldc homeoffice.wal-mart.com "adfind -hh <server> -rootdse currenttime"
+# alldc homeoffice.$domain "adfind -hh <server> -rootdse currenttime"
 
 # This will find errors with auth vs just ldap respons
-# alldc homeoffice.wal-mart.com "uptime \\<server>"
+# alldc homeoffice.$domain "uptime \\<server>"
 
 #Domain_IPConfigSheet.PS1 -ScriptMode FILE -File ComputerList.txt
 
